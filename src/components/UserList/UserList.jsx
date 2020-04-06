0cb819e5286b6caa53bd324c1b00972a114c349d
Junior <neo.junior.16@gmail.com>
2020-04-06 15:25:06 -0300

    Agregando email, detalles de tabla de usuarios

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -4,56 +4,77 @@ import './userlist.css'
 
 const UserList = () => {
   const [list, setList] = React.useState(results[0].clients)
+
   const showList = () => {
     const newList = list.map((client, index) => (
-      <tr key={index}>
-        <td className=''>
-          <div className='d-flex align-self-center'>
-            <img
-              className='rounded-circle mr-2'
-              src={client.avatar}
-              alt='Avatar'
-              width='50'
-              height='50'
-            />
-            <div className='align-self-center'>
+      <tr className='align-middle' key={index}>
+        <td className='d-flex align-items-center'>
+          <img className='avatar w-48' src={client.avatar} alt='Avatar' />
+          <div className='ml-3 align-middle text-left'>
+            <a
+              href='#'
+              className='text-primary'
+              data-abc='true'
+              onClick={() => seleccionUsuario(index)}
+            >
               {client.name.firstName} {client.name.lastName}
-            </div>
+            </a>
+            <br />
+            <small className='text-muted'>{client.name.jobTitle}</small>
           </div>
         </td>
-        <td>{client.document}</td>
-        <td>{client.login.email}</td>
-        <td>{client.addres.city}</td>
+        <td className='align-middle'>
+          <span class='badge badge-pill badge-primary'>Cliente</span>
+        </td>
+        <td className='align-middle'>{client.document}</td>
+        <td className='align-middle'>{client.login.email}</td>
+        <td className='align-middle'>{client.addres.city}</td>
+        <td className='align-middle'>{client.addres.county}</td>
       </tr>
     ))
     return newList
   }
-
+  const seleccionUsuario = id => {
+    console.log(id)
+  }
   return (
-    <div className='container card mt-4 bg-light border text-center'>
+    <div className='container card mt-4 bg-light border border-primary text-center'>
       <p className='display-4'>Lista de usuario</p>
-      <div className='btn-group'>
-        <button type='button' class='btn btn-primary'>
-          Administradores <span class='badge badge-light contador ml-2 align-middle badge-pill'>0</span>
+      <div className='row justify-content-start ml-2'>
+        <button type='button' className='btn btn-dark mr-2'>
+          Administradores
+          <span className='badge badge-light contador ml-2 align-middle badge-pill'>
+            0
+          </span>
         </button>
-        <button type='button' class='btn btn-primary'>
-          Agente de cuantas <span class='badge badge-light contador ml-2 align-middle badge-pill'>0</span>
+        <button type='button' className='btn btn-dark mr-2'>
+          Agente de cuantas
+          <span className='badge badge-light contador ml-2 align-middle badge-pill'>
+            0
+          </span>
         </button>
-        <button type='button' class='btn btn-primary'>
-  Clientes <span class='badge badge-light contador ml-2 align-middle badge-pill'>{list.length}</span>
+        <button type='button' className='btn btn-dark'>
+          Clientes
+          <span className='badge badge-light contador ml-2 align-middle badge-pill'>
+            {list.length}
+          </span>
         </button>
       </div>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Documento</th>
-            <th>Correo</th>
-            <th>Ciudad</th>
-          </tr>
-        </thead>
-        <tbody>{showList()}</tbody>
-      </table>
+      <div className='table-responsive'>
+        <table className='table table-borderless table-hover'>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Tipo</th>
+              <th>Documento</th>
+              <th>Correo</th>
+              <th>Ciudad</th>
+              <th>Provincia</th>
+            </tr>
+          </thead>
+          <tbody>{showList()}</tbody>
+        </table>
+      </div>
     </div>
   )
 }
